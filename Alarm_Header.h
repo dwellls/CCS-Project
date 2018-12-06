573cd82d898b30595711615a979a0a35f61a0c3b
davidbrinkmeier <45410693+davidbrinkmeier@users.noreply.github.com>
2018-12-06 01:01:16 -0500

    Update Alarm_Header.h

--- Alarm_Header.h
+++ Alarm_Header.h
@@ -27,14 +27,52 @@
 void BUTTONseconds(void);
 void SysTick_Initialize(void);
 void SysTick_delay(uint16_t delay);
+void ADC14_init(void);
+void PortADC_init(void);
+void SysTick_init_interrupt(void);
+void LCD_init(void);
+void LCD_nibble_write(unsigned char data, unsigned char control);
+void LCD_command(unsigned char command);
+void LCD_data(unsigned char data);
+void green_LED_pins(void);
+void green_LED_pins2(void);
+void SetupTimer32s();
+void TA_init(void);
+
 
 //Variable
-enum states
-{
-    INITIAL,
-    TIMEDISPLAY,
-    SETTIME,
-    SETALARM
-};
+volatile uint32_t timeout;
+static volatile uint16_t result;
+float nADC;
+float temp_c;
+float temp_f;
+char temp[10];
+char second[60];
+char minute[60];
+char hour[60];
+int i;
+int j;
+char time[12];
+char alarm[16];
+char alarmtime[12];
+
+//macros
+#define RS BIT1
+#define RW BIT2
+#define EN BIT3
+#define C4 261.63
+#define D4 293.66
+#define E4 329.63
+#define F4 349.23
+#define G4 392
+#define A4 440
+#define REST 0
+//Defining the lengths of notes
+#define QUARTER 1000000
+#define HALF 2000000
+#define BREATH_TIME 50000
+#define WHOLE 4000000
+#define MAX_NOTE 44 // How many notes are in the song below
+
 
 #endif /* ALARM_HEADER_H_ */
